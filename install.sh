f701a486f029ba8dbdc573220e108f2fc0a69f14
Kazuyoo_12 <dikinurrohmat42@gmail.com>
2024-12-15 18:32:25 +0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 SKIPMOUNT=false
 PROPFILE=true
-POSTFSDATA=true
+POSTFSDATA=false
 LATESTARTSERVICE=true
 
 REPLACE="
 "
 
 # array / variabel
-NAME="Celestial-Kernel | Kzyo"
-VERSION="1.2 Rebased"
+NAME="Celestial-Thermal | Kzyo"
+VERSION="1.0"
 ANDROIDVERSION=$(getprop ro.build.version.release)
-DATE="Sat 31 Aug 2024"
+DATE="Frid 29 Nov 2024"
 DEVICES=$(getprop ro.product.board)
 MANUFACTURER=$(getprop ro.product.manufacturer)
 API=$(getprop ro.build.version.sdk)
@@ -44,40 +44,14 @@ trim_partition () {
     sleep 0.1
 }
 
-# delete trash & log by @Bias_khaliq
-delete_trash_logs () {
-# Clear trash on /data/data
-for DIR in /data/data/*; do
-  if [ -d "${DIR}" ]; then
-    PACKAGE=$(echo ${DIR} | cut -d "/" -f 4)
-    rm -rf ${DIR}/cache/*
-    rm -rf ${DIR}/no_backup/*
-    rm -rf ${DIR}/app_webview/*
-    rm -rf ${DIR}/code_cache/*
-  fi
-done
-
-# Delete Logs
-rm -rf /data/{anr,log,tombstones,log_other_mode}/* \
-       /cache/*.{apk,tmp} \
-       /dev/log/* \
-       /data/system/{dropbox,usagestats,package_cache}/* \
-       /sys/kernel/debug/* \
-       /data/local/tmp* \
-       /data/dalvik-cache \
-       /data/media/0/{DCIM,Pictures,Music,Movies}/.thumbnails \
-       /data/media/0/{mtklog,MIUI/Gallery,MIUI/.debug_log,MIUI/BugReportCache} \
-       /data/vendor/thermal/{config,*.dump,*_history*.dump}
-}
-
 sleep 0.2
 ui_print ""
-ui_print "░█▀▀█ ── ░█─▄▀ ░█▀▀▀ ░█▀▀█ ░█▄─░█ ░█▀▀▀ ░█─── 
-░█─── ▀▀ ░█▀▄─ ░█▀▀▀ ░█▄▄▀ ░█░█░█ ░█▀▀▀ ░█─── 
-░█▄▄█ ── ░█─░█ ░█▄▄▄ ░█─░█ ░█──▀█ ░█▄▄▄ ░█▄▄█"
+ui_print "░█▀▀█ ── ▀▀█▀▀ ░█─░█ ░█▀▀▀ ░█▀▀█ ░█▀▄▀█ ─█▀▀█ ░█─── 
+░█─── ▀▀ ─░█── ░█▀▀█ ░█▀▀▀ ░█▄▄▀ ░█░█░█ ░█▄▄█ ░█─── 
+░█▄▄█ ── ─░█── ░█─░█ ░█▄▄▄ ░█─░█ ░█──░█ ░█─░█ ░█▄▄█"
 ui_print ""
 sleep 0.5
-ui_print " tweaks & improvements to the kernel."
+ui_print "    disable thermal throttling."
 ui_print ""
 sleep 0.2
 ui_print "***************************************"
@@ -95,11 +69,13 @@ sleep 0.2
 ui_print "- Manufacturer    : ${MANUFACTURER}"
 ui_print "***************************************"
 sleep 0.2
+ui_print "- Extracting module files"
+sleep 2
+unzip -o "$ZIPFILE" 'common/*' -d $MODPATH >&2
+unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
 ui_print "- Trimming up Partitions"
 sleep 2
 trim_partition &>/dev/null
-ui_print "- Delete trash and logs"
-delete_trash_logs
 sleep 0.5
 
 # Set permissions
